b973e5019ad30c52d5843152fbf3d062a6fae21c
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2015-03-08 17:49:56 +0100

    Added test for default exported object of compiler.js

--- src/test-runner/test/compile-spec.js
+++ src/test-runner/test/compile-spec.js
@@ -1,4 +1,5 @@
 import {ES5, ES6, CoffeeScript, TypeScript, compile} from '../compiler.js'
+import compiler from '../compiler.js'
 import {assert} from '../../_test-helper/assert';
 
 describe('compiler', function() {
@@ -6,6 +7,10 @@ describe('compiler', function() {
 	describe('ES5', function() {
 		it('exports the ES5 constant to the user', function() {
 			assert.notEqual(ES5, undefined);
+		});
+
+		it('default also exports the ES5 constant to the user', function() {
+			assert.notEqual(compiler.ES5, undefined);
 		});
 
 		it('can compile ES5 code', function() {
@@ -20,6 +25,10 @@ describe('compiler', function() {
 			assert.notEqual(ES6, undefined);
 		});
 
+		it('default also exports the ES6 constant to the user', function() {
+			assert.notEqual(compiler.ES6, undefined);
+		});
+
 		it('can compile ES6 code', function() {
 			assert.doesNotThrow(function() {
 				compile(ES6, 'var x = 1;');
@@ -32,6 +41,10 @@ describe('compiler', function() {
 			assert.notEqual(CoffeeScript, undefined);
 		});
 
+		it('default also exports the CoffeeScript constant to the user', function() {
+			assert.notEqual(compiler.CoffeeScript, undefined);
+		});
+
 		it('can compile CoffeeScript code', function() {
 			assert.doesNotThrow(function() {
 				compile(CoffeeScript, 'x = 1;');
@@ -42,6 +55,10 @@ describe('compiler', function() {
 	describe('TypeScript', function() {
 		it('exports the TypeScript constant to the user', function() {
 			assert.notEqual(TypeScript, undefined);
+		});
+
+		it('default also exports the TypeScript constant to the user', function() {
+			assert.notEqual(compiler.TypeScript, undefined);
 		});
 
 		it('can compile TypeScript code', function() {
