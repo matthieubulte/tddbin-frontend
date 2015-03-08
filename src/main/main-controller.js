e1ade6540364bba501f7f67be91d704647246bec
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2015-03-08 14:54:28 +0100

    Added support for CoffeeScript

--- src/main/main-controller.js
+++ src/main/main-controller.js
@@ -16,7 +16,8 @@ export function Controller(domNode, config) {
 
   this.availableLanguages = [
     { language: compiler.ES5, aceMode: 'ace/mode/javascript' },
-    { language: compiler.ES6, aceMode: 'ace/mode/javascript' }
+    { language: compiler.ES6, aceMode: 'ace/mode/javascript' },
+    { language: compiler.CoffeeScript, aceMode: 'ace/mode/coffee'}
   ];
 
   this.render();
